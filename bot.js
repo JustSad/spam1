11d836a353f36068f20fda5951f9e42a942fbd03
JustSad <44894900+JustSad@users.noreply.github.com>
2018-12-11 04:03:35 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "520585062517833728"; // ايدي السررفر
+var channel = "522018095326298112";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,**')
     },305);
 })
 
